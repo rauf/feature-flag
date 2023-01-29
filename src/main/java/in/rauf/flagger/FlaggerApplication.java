934ef37f5274ab2cc86385de56a5e6b08b879d34
Abdul Rauf <abdul3rauf@gmail.com>
2023-01-29 14:42:12 +0530

    initial commit

---
+++ src/main/java/in/rauf/flagger/FlaggerApplication.java
@@ -0,0 +1,13 @@
+package in.rauf.flagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlaggerApplication.class, args);
+	}
+
+}
